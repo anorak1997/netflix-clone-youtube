3e45266342d40df91727ef8b46ef3b110380c8a9
anorak1997 <siddharthadubey.1997@gmail.com>
2023-11-08 10:48:54 +0530

    First commit with the youtube link

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYtPsVVU6QyX-ISaCJNjySr1WdPNQMmmk",
+    authDomain: "netflix-clone-24c5c.firebaseapp.com",
+    projectId: "netflix-clone-24c5c",
+    storageBucket: "netflix-clone-24c5c.appspot.com",
+    messagingSenderId: "566436383603",
+    appId: "1:566436383603:web:4dd7f6d71659f39aa65306"
+};
