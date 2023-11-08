1e3052d727f0ec8032839c4c313f7bd697155ea2
anorak1997 <siddharthadubey.1997@gmail.com>
2023-11-08 11:06:13 +0530

    Added VS Code with github

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
       <h2>This is the titorial I am following to make the following project.</h2>
       <h2>  Youtube Link: https://www.youtube.com/watch?v=HW5roUF2RLg&t=158s 
       </h2>
+      <h2>Made changes to github and added repository and added github to my vs studio code IDE.</h2>
+      <h2>Need to check that out and understand it.</h2>
     </div>
   );
 }
